ca4f35728b8cdb72a30be03ac21b7552440a4968
Pola Lucashevski <polalucash@gmail.com>
2019-12-01 17:37:17 +0200

    final

--- ClothesShop/Controllers/ShopController.cs
+++ ClothesShop/Controllers/ShopController.cs
@@ -7,70 +7,56 @@ using ClothesShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ClothesShop.Controllers
-{
-    [Route("[api/controller]")]
-    [ApiController]
-    public class ShopController : ControllerBase
-    {
+namespace ClothesShop.Controllers{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ShopController: ControllerBase{
 		private readonly ClothesShopContext _context;
-		public ShopController(ClothesShopContext context)
-		{
+
+		public ShopController(ClothesShopContext context) {
 			_context = context;
 		}
-		
+
 
 		// GET: shop/Products
 		[HttpGet("Products")]
-		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
-		{
+		public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync() {
 			return await _context.Product
 				.AsNoTracking()
 				.ToListAsync();
 		}
 
 		// GET: shop/Products/5
-		[HttpGet("Product={id}")]
-		public async Task<ActionResult<Product>> GetProduct(int id)
-		{
-			var product = await _context.Product.FindAsync(id);
-
-			if (product == null)
-			{
+		[HttpGet("Product={productId}")]
+		public async Task<ActionResult<Product>> GetProductAsync(int productId) {
+			var product = await _context.Product.FindAsync(productId);
+			
+			if(product == null)
 				return NotFound();
-			}
-
 			return product;
 		}
 
 		// GET: shop/Purchases
 		[HttpGet("Purchases")]
-		public async Task<ActionResult<IEnumerable<Purchase>>> GetPurchase()
-		{
+		public async Task<ActionResult<IEnumerable<Purchase>>> GetPurchasesAsync() {
 			return await _context.Purchase.ToListAsync();
 		}
 
 		// GET: shop/Purchases/5
-		[HttpGet("Purchases={id}")]
-		public async Task<ActionResult<Purchase>> GetPurchase(int id)
-		{
-			var purchase = await _context.Purchase.FindAsync(id);
+		[HttpGet("Purchases={purchaseId}")]
+		public async Task<ActionResult<Purchase>> GetPurchaseAsync(int purchaseId) {
+			var purchase = await _context.Purchase.FindAsync(purchaseId);
 
-			if (purchase == null)
-			{
+			if(purchase == null) 
 				return NotFound();
-			}
 
 			return purchase;
 		}
+
 		// GET: shop/statistics?year=2019&month=12
 		[HttpGet("statistics")]
-		public ActionResult<IEnumerator<MonthlyStatisticRecord>> GetMonthlyStatistics(int year, int month)
-		{
-			if (year < 1 || month < 1 || month > 12 || year > DateTime.Today.Year)
-			{
-				return BadRequest();
-			}
+		public ActionResult<IEnumerator<MonthlyStatisticRecord>> GetMonthlyStatistics(int year, int month) {
+			if(year < 1 || month < 1 || month > 12 || year > DateTime.Today.Year) return BadRequest();
 
 			var dailyPurchases = _context.Purchase
 				.Where(r => r.PurchaseDate.Month == month
@@ -98,69 +84,63 @@ namespace ClothesShop.Controllers
 				}).GetEnumerator());
 		}
 
-		[HttpPost("PurchaseProduct")]
-		public async Task<IActionResult> PurchaseProductAsync(int id)
+		[HttpPost("Product")]
+		public async Task<ActionResult<Product>> CreateProductAsync(Product product)
 		{
+			_context.Product.Add(product);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetProductAsync", new { id = product.ProductId }, product);
+		}
+
+		[HttpPost("PurchaseProduct")]
+		public async Task<IActionResult> PurchaseProductAsync(int productId) {
 			var product = await _context.Product
-				.Include(r=>r.Purchases)
-				.FirstOrDefaultAsync(r=>r.ProductId==id);
+				.Include(r => r.Purchases)
+				.FirstOrDefaultAsync(r => r.ProductId == productId);
 
-			if(product == null) {
-				return NotFound();
-			}
+			if(product == null) return NotFound();
 
-			if (product.Quantity <= 0) {
-				return BadRequest();
-			}
+			if(product.Quantity <= 0) return BadRequest();
 
 			var purchase = product.Purchase();
 			_context.Entry(product).State = EntityState.Modified;
 
-			try
-			{
+			try {
 				await _context.SaveChangesAsync();
 			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!ProductExists(id))
-				{
-					return NotFound();
-				}
+			catch(DbUpdateConcurrencyException) {
+				if(!ProductExists(productId)) return NotFound();
 				throw;
 			}
-			return CreatedAtAction("GetPurchase", new { id = purchase.PurchaseId }, purchase);
+
+			return CreatedAtAction("GetPurchasesAsync", new { purchaseId = purchase.PurchaseId}, purchase);
 		}
 
 		[HttpPut("ReturnPurchase")]
-		public async Task<IActionResult> ReturnPurchaseAsync(int id)
-		{
+		public async Task<IActionResult> ReturnPurchaseAsync(int purchaseId) {
 			var purchase = await _context.Purchase
 				.Include(r => r.Product)
-				.FirstOrDefaultAsync(r => r.PurchaseId == id);
+				.FirstOrDefaultAsync(r => r.PurchaseId == purchaseId);
 
-			if (purchase == null)
+			if(purchase == null)
 				return NotFound();
 
-			if (!purchase.Returnable || !purchase.Product.Returnable)
-				return BadRequest(new { message = "The product is not returnable" });
-			
-			if (DateTime.UtcNow.Subtract(purchase.PurchaseDate).Days > 30)
-				return BadRequest(new { message = "Over 30 days, return not accepted" });
+			if(!purchase.Returnable || !purchase.Product.Returnable)
+				return BadRequest(new {message = "The product is not returnable"});
+
+			if(DateTime.UtcNow.Subtract(purchase.PurchaseDate).Days > 30)
+				return BadRequest(new {message = "Over 30 days, return not accepted"});
 
 			purchase.Return();
 			_context.Entry(purchase).State = EntityState.Modified;
 			_context.Entry(purchase.Product).State = EntityState.Modified;
 
-			try
-			{
+			try {
 				await _context.SaveChangesAsync();
 			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!ProductExists(id))
-				{
-					return NotFound();
-				}
+			catch(DbUpdateConcurrencyException) {
+				if(!ProductExists(purchaseId)) return NotFound();
 				throw;
 			}
 
@@ -168,48 +148,37 @@ namespace ClothesShop.Controllers
 				? "Under 15 days, cash return"
 				: "Between 15 to 30 days, 'check' return";
 
-			return Accepted(new{ message, purchase });
+			return Accepted(new {message, purchase});
 		}
 
 		[HttpPut("{id}")]
-		public async Task<IActionResult> UpdateProductQuantityAsync(int id, Product product)
-		{
-			if (product == null || id <= 0 || product.Quantity < 0) {
+		public async Task<IActionResult> UpdateProductQuantityAsync(int productId, Product product) {
+			
+			if(product == null || productId <= 0 || product.Quantity < 0)
 				return BadRequest();
-			}
 
 			var productToUpdate = await _context.Product
 				.AsTracking()
-				.FirstOrDefaultAsync(r => r.ProductId == id);
+				.FirstOrDefaultAsync(r => r.ProductId == productId);
 
 			productToUpdate.Quantity = product.Quantity;
 			_context.Entry(productToUpdate).State = EntityState.Modified;
 
-			try
-			{
+			try {
 				await _context.SaveChangesAsync();
 			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!ProductExists(id))
-				{
+			catch(DbUpdateConcurrencyException) {
+				if(!ProductExists(productId)) 
 					return NotFound();
-				}
 				throw;
 			}
+
 			return NoContent();
 		}
-		[HttpPost("Product")]
-		public async Task<ActionResult<Product>> CreateProduct(Product product)
-		{
-			_context.Product.Add(product);
-			await _context.SaveChangesAsync();
 
-			return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
-		}
-		private bool ProductExists(int id)
-		{
-			return _context.Product.Any(e => e.ProductId == id);
+
+		private bool ProductExists(int productId) {
+			return _context.Product.Any(e => e.ProductId == productId);
 		}
 	}
 }
